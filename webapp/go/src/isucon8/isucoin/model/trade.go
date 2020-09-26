b5552a81636245d446a804b545157445f6cb35d2
masami <msihima0304@gmail.com>
2020-09-26 22:37:56 +0900

    feat: 🎸 非同期で投げてみる

--- webapp/go/src/isucon8/isucoin/model/trade.go
+++ webapp/go/src/isucon8/isucoin/model/trade.go
@@ -181,13 +181,17 @@ func commitReservedOrder(tx *sql.Tx, order *Order, targets []*Order, reserves []
 			"trade_id": tradeID,
 		})
 	}
-	bank, err := Isubank(tx)
-	if err != nil {
-		return errors.Wrap(err, "isubank init failed")
-	}
-	if err = bank.Commit(reserves); err != nil {
-		return errors.Wrap(err, "commit")
-	}
+	func () error {
+		bank, err := Isubank(tx)
+		if err != nil {
+			return errors.Wrap(err, "isubank init failed")
+		}
+		if err = bank.Commit(reserves); err != nil {
+			return errors.Wrap(err, "commit")
+		}
+		return nil
+	}()
+	
 	return nil
 }
 
@@ -264,6 +268,7 @@ func tryTrade(tx *sql.Tx, orderID int64) error {
 	if err = commitReservedOrder(tx, order, targets, reserves); err != nil {
 		return err
 	}
+
 	reserves = reserves[:0]
 	return nil
 }
