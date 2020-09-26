c3540e4def1842a456cc913af20803be19c0443c
isucon.user <mishima0304@gmail.com>
2020-09-26 05:43:04 +0000

    sqlを消す

--- webapp/sql/01_create_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'isucon'@'%' IDENTIFIED WITH mysql_native_password BY 'isucon';
-GRANT ALL ON isucoin.* TO 'isucon'@'%';
-CREATE USER 'isucon'@'localhost' IDENTIFIED WITH mysql_native_password BY 'isucon';
-GRANT ALL ON isucoin.* TO 'isucon'@'localhost';
